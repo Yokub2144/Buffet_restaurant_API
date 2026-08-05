b19115472cac1735e649dd247fa1742d54f24c21
aof20022002 <153268994+aof20022002@users.noreply.github.com>
2026-08-06 00:55:04 +0700

    ปริ้นบิล

--- Dtos/OrderDtos.cs
+++ Dtos/OrderDtos.cs
@@ -1,10 +1,14 @@
 
 namespace Buffet_Restaurant_API.Dtos
 {
-public class OrderDto
+    public class OrderDto
     {
         public int CartId { get; set; }
         public int BillId { get; set; }
         public string OrderType { get; set; } = string.Empty;
     }
+}
+public class UpdateStatusDto
+{
+    public string Status { get; set; } = string.Empty;
 }
