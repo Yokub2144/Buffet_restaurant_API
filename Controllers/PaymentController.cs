2477c5d0dc987af6a46f847cd475fb554ec919a6
aof20022002 <153268994+aof20022002@users.noreply.github.com>
2026-08-10 15:15:40 +0700

    fix: improve error handling in payment type update process

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -338,7 +338,6 @@ namespace Buffet_Restaurant_Managment_System_API.Controllers
                 });
             }
             catch (Exception ex)
-
             {
                 return StatusCode(500, new { message = "เกิดข้อผิดพลาดในการอัปเดตประเภทการชำระเงิน", error = ex.Message });
             }
